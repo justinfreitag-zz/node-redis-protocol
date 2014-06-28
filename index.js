cd490e9798165bf73ab36229a83cfd4527dbeb97
Justin Freitag <justin.freitag@gmail.com>
2014-06-28 21:58:58 +1000

    fix to append buffer logic

--- index.js
+++ index.js
@@ -3,76 +3,55 @@
 var events = require('eventemitter3');
 var util = require('util');
 
-// TODO add binary support
-exports.createRequest = function () {
-  throw new Error('Not implemented');
-  return buffer;
-};
-
-exports.createRequestString = function () {
-  var request = '*' + arguments.length + '\r\n';
-  for (var i = 0; i < arguments.length; i++) {
-    if (typeof arguments[i] === 'string') {
-      request += '$' + arguments[i].length + '\r\n' + arguments[i] + '\r\n';
-    } else {
-      var s = '' + arguments[i];
-      request += '$' + s.length + '\r\n' + s + '\r\n';
-    }
-  }
-  return request;
-};
-
-function decodeString(buffer, begin, end) {
+function decodeString(buffer, begin, length) {
   var tmp = '';
-  for (var i = begin; i < end; i++) {
-    tmp += String.fromCharCode(buffer[i]);
+  while (length--) {
+    tmp = String.fromCharCode(buffer[begin + length]) + tmp;
   }
   return tmp;
 }
 
-function copyBuffer(buffer, begin, end) {
+function copyBuffer(buffer, begin, length) {
   // TODO add binary support
-  if ((end - begin) < 256) {
-    return decodeString(buffer, begin, end);
+  if (length < 2048) {
+    return decodeString(buffer, begin, length);
   }
-  return buffer.toString('utf-8', begin, end);
+  return buffer.toString('utf8', begin, begin + length);
 }
 
-// TODO add check for max response length
 function seekTerminator(parser) {
-  var offset = parser.offset + 1;
+  var offset = parser.offset;
   var length = parser.buffer.length;
-  while (parser.buffer[offset] !== 13 && parser.buffer[offset + 1] !== 10) {
-    ++offset;
-    if (offset >= length) {
-      return null;
+  while (++offset !== length) {
+    if (parser.buffer[offset] === 13 && parser.buffer[offset + 1] === 10) {
+      length = offset - parser.offset;
+      parser.offset = offset + 2;
+      return length;
     }
   }
-  parser.offset = offset + 2;
-  return offset;
 }
 
 function parseLength(parser) {
   var begin = parser.offset;
-  var end = seekTerminator(parser);
-  if (end === null) {
-    return null;
+  var length = seekTerminator(parser);
+  if (length === undefined) {
+    return;
   }
-  return parseInt(decodeString(parser.buffer, begin, end), 10);
+  return parseInt(decodeString(parser.buffer, begin, length), 10);
 }
 
 function parseSimpleString(parser) {
   var begin = parser.offset;
-  var end = seekTerminator(parser);
-  if (end === null) {
-    return null;
+  var length = seekTerminator(parser);
+  if (length === undefined) {
+    return;
   }
-  return copyBuffer(parser.buffer, begin, end);
+  return copyBuffer(parser.buffer, begin, length);
 }
 
 function parseError(parser) {
   var response = parseSimpleString(parser);
-  if (response !== null) {
+  if (response !== undefined) {
     response = new Error(response);
   }
   return response;
@@ -80,53 +59,46 @@ function parseError(parser) {
 
 function parseBulkString(parser) {
   var length = parseLength(parser);
+  if (length === undefined) {
+    return;
+  }
   if (length === null) {
     return null;
   }
-  if (length === -1) {
-    return -1;
-  }
-
   var begin = parser.offset;
-  var end = parser.offset + length;
-  if ((end + 2) > parser.buffer.length) {
-    return null;
+  parser.offset = begin + length + 2;
+  if (parser.offset > parser.buffer.length) {
+    return;
   }
-  parser.offset = end + 2;
-  return copyBuffer(parser.buffer, begin, end);
+  return copyBuffer(parser.buffer, begin, length);
 }
 
 function parseInteger(parser) {
   var begin = parser.offset;
-  var end = seekTerminator(parser);
-  if (end === null) {
-    return null;
+  var length = seekTerminator(parser);
+  if (length === undefined) {
+    return;
   }
-  return parseInt(decodeString(parser.buffer, begin, end), 10);
+  return parseInt(decodeString(parser.buffer, begin, length), 10);
 }
 
 function parseArray(parser) {
   var length = parseLength(parser);
-  if (length === null) {
-    return null;
+  if (length === undefined) {
+    return;
   }
   if (length === -1) {
-    return -1;
+    return null;
   }
-
   var responses = new Array(length);
-
   for (var i = 0; i < length; i++) {
     if ((parser.offset + 4) > parser.buffer.length) {
-      return null;
+      return;
     }
     var type = parser.buffer[parser.offset++];
     var response = parseType(parser, type);
-    if (response === null) {
-      return null;
-    }
-    if (response === -1) {
-      response = null;
+    if (response === undefined) {
+      return;
     }
     responses[i] = response;
   }
@@ -138,8 +110,6 @@ function handleError(parser, error) {
   if (!parser.emit('error', error)) {
     throw error;
   }
-
-  return null;
 }
 
 function parseType(parser, type) {
@@ -163,19 +133,22 @@ function parseType(parser, type) {
 }
 
 function appendBuffer(parser, buffer) {
-  if (parser.buffer === null || parser.offset >= parser.buffer.length) {
+  if (!parser.buffer) {
     parser.buffer = buffer;
-  } else {
-    var length = (parser.buffer.length - parser.offset) + buffer.length;
-    if (length > parser.options.maxBufferLength) {
-        handleError(this, new Error('Maximum buffer length exceeded'));
-        return;
-    }
-    var newBuffer = new Buffer(length);
-    parser.buffer.copy(newBuffer, 0, parser.offset, parser.buffer.length);
-    buffer.copy(newBuffer, parser.buffer.length, 0, buffer.length);
-    parser.buffer = newBuffer;
+    parser.offset = 0;
+    return;
   }
+  var oldLength = parser.buffer.length;
+  var remainingLength = oldLength - parser.offset;
+  var newLength = remainingLength + buffer.length;
+  if (newLength > parser.options.maxBufferLength) {
+    handleError(this, new Error('Maximum buffer length exceeded'));
+    return;
+  }
+  var newBuffer = new Buffer(newLength);
+  parser.buffer.copy(newBuffer, 0, parser.offset, oldLength);
+  buffer.copy(newBuffer, remainingLength, 0, buffer.length);
+  parser.buffer = newBuffer;
   parser.offset = 0;
 }
 
@@ -198,24 +171,37 @@ ResponseParser.DEFAULT_OPTIONS = DEFAULT_OPTIONS;
 ResponseParser.prototype.parse = function (buffer) {
   appendBuffer(this, buffer);
 
-  while ((this.offset + 4) <= this.buffer.length) {
-    var offset = this.offset;
-    var type = this.buffer[this.offset++];
-    var response = parseType(this, type);
-    if (response === null) {
-      this.offset = offset;
-      break;
-    }
-    if (response === -1) {
-      response = null;
+  var length = this.buffer.length;
+  do {
+    var begin = this.offset;
+    var response = parseType(this, this.buffer[this.offset++]);
+    if (response === undefined) {
+      this.offset = begin;
+      return;
     }
     this.emit('response', response);
-  }
+  } while (this.offset !== length);
 
-  if (this.offset >= this.buffer.length) {
-    this.buffer = null;
-  }
+  this.buffer = null;
 };
 
 exports.ResponseParser = ResponseParser;
+
+// TODO add binary support
+exports.createRequest = function () {
+  throw new Error('Not yet implemented');
+};
+
+exports.createRequestString = function () {
+  var request = '*' + arguments.length + '\r\n';
+  for (var i = 0; i < arguments.length; i++) {
+    if (typeof arguments[i] === 'string') {
+      request += '$' + arguments[i].length + '\r\n' + arguments[i] + '\r\n';
+    } else {
+      var s = '' + arguments[i];
+      request += '$' + s.length + '\r\n' + s + '\r\n';
+    }
+  }
+  return request;
+};
 
